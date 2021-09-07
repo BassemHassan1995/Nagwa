e8565d2c4fc77cb14f2fec73ac5340e19c5de79a
Bassem Hassan <bassem.hassan1995@gmail.com>
2021-09-07 14:21:03 +0200

    Initial commit

---
+++ app/src/main/java/bassem/bm/task/nagwa/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package bassem.bm.task.nagwa.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import bassem.bm.task.nagwa.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
