a4340195faa219ba3f26d6aa773cecdab300e334
Bassem Hassan <bassem.hassan1995@gmail.com>
2021-09-07 15:07:51 +0200

    add list fragment

---
+++ app/src/main/java/bassem/bm/task/nagwa/ui/list/ListFragment.kt
@@ -0,0 +1,18 @@
+package bassem.bm.task.nagwa.ui.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import bassem.bm.task.nagwa.databinding.FragmentListBinding
+import bassem.bm.task.nagwa.ui.SharedViewModel
+import bassem.bm.task.nagwa.ui.base.BaseFragment
+
+class ListFragment : BaseFragment<FragmentListBinding>() {
+
+    override val viewModel by activityViewModels<SharedViewModel>()
+
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentListBinding = FragmentListBinding.inflate(layoutInflater, container, false)
+}
